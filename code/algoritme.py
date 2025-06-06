350caf53420ff843a451514368f1b21a5d33f2a0
IllicitPrism <65886482+EiseZ@users.noreply.github.com>
2025-06-06 16:23:33 +0200

    Working code to create a focus point + cropped the beam with some margin

--- code/algoritme.py
+++ code/algoritme.py
@@ -1,9 +1,9 @@
-
 #%%
 
 import numpy as np
 from pypylon import pylon
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 from slmsuite.hardware.slms.meadowlark import Meadowlark
 s = Meadowlark(sdk_path="C:/Program Files/Meadowlark Optics/Blink 1920 HDMI", verbose=True, wav_um=0.633)
@@ -16,27 +16,32 @@ beeld_h = 1536
 beeld = np.empty((beeld_h, beeld_b)) # Vector van intensiteiten van het beeld per pixel
 lcd_b = s.shape[1]
 lcd_h = s.shape[0]
-padding_top = 300
+padding_top = 200
 padding_bottom = 400
-padding_left = 720
-padding_right = 700
+padding_left = 520
+padding_right = 800
+#padding_top = 120
+#padding_bottom = 240
+#padding_left = 480
+#padding_right = 840
 slm_b = lcd_b - padding_left - padding_right
 slm_h = lcd_h - padding_bottom - padding_top
 slm = np.empty((slm_h, slm_b)) # Vector van alle phasen van de pixels van de SLM
-segment_pixels = 50*50 # Aantal pixels in een segment, moet een kwadraat zijn en segment_length moet slm_b & slm_h delen
+segment_pixels = 30*30 # Aantal pixels in een segment, moet een kwadraat zijn en segment_length moet slm_b & slm_h delen
 segment_length = int(np.sqrt(segment_pixels))
 n_segments = (slm_b * slm_h) // segment_pixels
 print(n_segments)
 
 # Coordinaten van het focus punt
 focus_x = 1000
-focus_y = 800
+focus_y = 870
 
 # Uiteindelijke voltages voor de slm voor de beste phasen
 slm_segments_best_phase = np.zeros((slm_h // segment_length, slm_b // segment_length))
+slm_segments_best_intensity = np.zeros((slm_h // segment_length, slm_b // segment_length))
 slm_final = np.empty((slm_h, slm_b))
 
-phases = [0, np.pi / 2, (np.pi * 3) / 2] # De phasen die we willen testen voor elk segment
+phases = [2 * np.pi, np.pi / 2, (np.pi * 3) / 2] # De phasen die we willen testen voor elk segment
 
 camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
 camera.Open()
@@ -63,36 +68,48 @@ def get_image():
     camera.StopGrabbing()
     return img
 
-def show_image(img):
-    
-    plt.matshow(img)
-    plt.xlim(800, 1200)
-    plt.ylim(600, 1000)
+def show_image(img, seg_x, seg_y, phase):
+    fig, axes = plt.subplots(1, 2)
+    axes[0].matshow(img)
+    axes[0].set_xlim(800, 1200)
+    axes[0].set_ylim(600, 1000)
+    axes[0].set_title(f"x: {seg_x}, y: {seg_y}, phi: {phase}")
+    axes[1].matshow(slm_segments_best_intensity)
     plt.show()
     return
 
-for seg_row in range(0, slm_h // segment_length): # Itereer over elk segment
+def set_matrix(mat, val, from_x, to_x, from_y, to_y):
+    for x in range(from_x, to_x):
+        for y in range(from_y, to_y):
+            mat[x][y] = val
+    return mat
+
+for seg_row in tqdm(range(0, slm_h // segment_length)): # Itereer over elk segment
     for seg_column in range(0, slm_b // segment_length):
         best_phase = 0 # Beste phase nu bekend
         best_intensity = 0 # De intensiteit die daarbij hoort
         for phase in phases:
-            slm[seg_row * segment_length : seg_row * segment_length + segment_length][seg_column * segment_length : seg_column * segment_length + segment_length] = phase
-            print(seg_row, seg_column, ":", phase)
+            set_matrix(slm, phase, seg_row * segment_length, seg_row * segment_length + segment_length, seg_column * segment_length,  seg_column * segment_length + segment_length)
             s.set_phase(padd_slm(slm), settle=True)
-            show_image(beeld)
             beeld = get_image()
-            if beeld[focus_y][focus_x] > best_intensity: # Als de phase beter werkt, noteer dit dan, anders negeren we het
+            show_image(beeld, seg_column, seg_row, phase)
+            if beeld[focus_y][focus_x] + beeld[focus_y - 1][focus_x] + beeld[focus_y + 1][focus_x] + beeld[focus_y - 1][focus_x - 1] + beeld[focus_y + 1][focus_x - 1]  + beeld[focus_y - 1][focus_x + 1] + beeld[focus_y + 1][focus_x + 1] + beeld[focus_y][focus_x - 1] + beeld[focus_y][focus_x + 1] > best_intensity: # Als de phase beter werkt, noteer dit dan, anders negeren we het
                 best_phase = phase
-                best_intensity = beeld[focus_y][focus_x]
+                best_intensity = beeld[focus_y][focus_x] + beeld[focus_y - 1][focus_x] + beeld[focus_y + 1][focus_x] + beeld[focus_y - 1][focus_x - 1] + beeld[focus_y + 1][focus_x - 1]  + beeld[focus_y - 1][focus_x + 1] + beeld[focus_y + 1][focus_x + 1] + beeld[focus_y][focus_x - 1] + beeld[focus_y][focus_x + 1]
         # Sla de beste phase op
         slm_segments_best_phase[seg_row][seg_column] = best_phase
-        slm_final[seg_row * segment_length : seg_row * segment_length + segment_length][seg_column * segment_length : seg_column * segment_length + segment_length] = best_phase 
-        
+        slm_segments_best_intensity[seg_row][seg_column] = best_intensity
+        set_matrix(slm_final, best_phase, seg_row * segment_length, seg_row * segment_length + segment_length, seg_column * segment_length,  seg_column * segment_length + segment_length)
+
         # Reset slm
         slm = np.empty((slm_h, slm_b))
 
 
 s.set_phase(padd_slm(slm_final), settle=True)
 beeld = get_image()
-show_image(beeld)
+show_image(beeld, 0, 0, 0)
+plt.matshow(slm_segments_best_intensity)
+plt.show()
+s.plot()
+plt.show()
 camera.Close()
